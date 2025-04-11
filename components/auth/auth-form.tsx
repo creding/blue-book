b7767fb17c2e67cd85d57fb76411e48c6434b48a
Christopher Reding <christopher.reding@mindrally.com>
2025-04-11 00:47:22 -0500

    Fix url

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -6,9 +6,9 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Box, Paper } from "@mantine/core";
 
 export default function AuthForm({ redirectPath }: { redirectPath: string }) {
-  const redirectTo = `${
-    process.env.NEXT_PUBLIC_BASE_URL
-  }/auth/callback?redirect=${encodeURIComponent(redirectPath)}`;
+  const redirectTo = `https://blue-book-ashy.vercel.app/auth/callback?redirect=${encodeURIComponent(
+    redirectPath
+  )}`;
 
   const supabase = createClientComponentClient();
 
