bea6d0960935800bdf7dc733966461609b0ac30d
Christopher Reding <christopher.reding@mindrally.com>
2025-04-11 00:26:48 -0500

    Fixed build errors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
-import AuthForm from '@/components/auth/auth-form'
-import { Title, Container } from '@mantine/core'
+import AuthForm from "@/components/auth/auth-form";
+import { Title, Container, Card, Center } from "@mantine/core";
 
 export default function LoginPage({
   searchParams,
@@ -7,11 +7,23 @@ export default function LoginPage({
   searchParams: { redirect?: string };
 }) {
   return (
-    <Container size="xs">
-      <Title order={2} ta="center" mt="xl" mb="lg">
-        Welcome to The Blue Book
-      </Title>
-      <AuthForm />
+    <Container size="sm" my="lg">
+      <Center h="100vh">
+        <Card
+          withBorder
+          shadow="md"
+          pl="xl"
+          pr="xl"
+          pt="sm"
+          pb="xl"
+          radius="md"
+        >
+          <Title order={2} ta="center" mt="xl" mb="lg">
+            Welcome to The Blue Book
+          </Title>
+          <AuthForm redirectPath={searchParams.redirect || "/"} />
+        </Card>
+      </Center>
     </Container>
-  )
+  );
 }
